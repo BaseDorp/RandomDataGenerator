a719916095c057e00db9ab5a192d820532695b28
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2019-09-14 15:46:35 -0500

    Random Data Generator
    Creates random guids and doubles and prints them off in a text file in the program's folders. Part of my Algorithm's class homework

---
+++ InClassAlgorithmsD1/GuidDataGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+namespace InClassAlgorithmsD1
+{
+    public class GuidDataGenerator
+    {
+        public List<Guid> myGuids;
+        public List<double> myDoubles;
+
+        public GuidDataGenerator()
+        {
+            // Creates list of Guids
+            myGuids = new List<Guid>();
+            myDoubles = new List<double>();
+            Random random = new Random();
+
+            
+            // Gets 1 milion guids -----------------------------------------------------------------
+            for (int i = 0; i < 1000000; i++)
+            {
+                // Get a random Guid
+                myGuids.Add(Guid.NewGuid());
+                // Get random double
+                myDoubles.Add(random.NextDouble());
+            }
+
+            // Prints to screen -------------------------------------------------------------------
+            
+            for (int i = 0; i < myGuids.Count; i++)
+            {
+                Console.Write(i + ", " + myGuids[i] + ", " + myDoubles[i] + "\n");
+            }
+
+            // Create txt document --------------------------------------------------------------
+            string filePath = Directory.GetCurrentDirectory();
+            // takes the current directory that the project is in, and creates a txt file
+            filePath += @"\DataText.txt";
+
+            StreamWriter sw = new StreamWriter(filePath);
+
+            for (int i = 0; i < myGuids.Count; i++)
+            {
+                sw.Write("{0}. {1}, {2}\n", i, myGuids[i], myDoubles[i]);
+            }
+            
+        }
+    }
+}
--- InClassAlgorithmsD1/Program.cs
+++ InClassAlgorithmsD1/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+/*
+ * Group Members:
+ * Sam Hirsch, Sneh Mehta, David Nichol
+ */
+
 namespace InClassAlgorithmsD1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GuidDataGenerator test = new GuidDataGenerator();
+            
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
         }
     }
 }
